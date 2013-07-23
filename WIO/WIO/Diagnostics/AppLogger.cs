35bcdfb13546f6a4909c7d4ef76ccdee0b4f6f87
Geoff Hudik <thnk2wn2@gmail.com>
2013-07-22 20:58:17 -0700

    ability to configure amount of logging done

--- WIO/WIO/Diagnostics/AppLogger.cs
+++ WIO/WIO/Diagnostics/AppLogger.cs
@@ -1,4 +1,5 @@
 ﻿using NLog;
+using WIO.Settings;
 
 namespace WIO.Diagnostics
 {
@@ -8,6 +9,7 @@ namespace WIO.Diagnostics
 
         public new void Debug(string format, params object[] args)
         {
+            if (!AppSettings.Instance.Log.IsDebugEnabled) return;
             base.Debug(format, args);
             //LoggrNetLogger.LogDebug(new LogMsg(string.Format(format, args)));
             LogglyLogger.Debug(format, args);
@@ -15,18 +17,21 @@ namespace WIO.Diagnostics
 
         public new void Error(string format, params object[] args)
         {
+            if (!AppSettings.Instance.Log.IsErrorEnabled) return;
             base.Error(format, args);
             LoggrNetLogger.LogError(new LogMsg(string.Format(format, args)));
         }
 
         public new void Fatal(string format, params object[] args)
         {
+            if (!AppSettings.Instance.Log.IsFatalEnabled) return;
             base.Fatal(format, args);
             LoggrNetLogger.LogError(new LogMsg(string.Format(format, args)));
         }
 
         public new void Info(string format, params object[] args)
         {
+            if (!AppSettings.Instance.Log.IsInfoEnabled) return;
             base.Info(format, args);
             //LoggrNetLogger.LogInfo(new LogMsg(string.Format(format, args)));
             LogglyLogger.Info(format, args);
@@ -34,18 +39,21 @@ namespace WIO.Diagnostics
 
         public new void Trace(string format, params object[] args)
         {
+            if (!AppSettings.Instance.Log.IsDebugEnabled) return;
             base.Trace(format, args);
             LoggrNetLogger.LogDebug(new LogMsg(string.Format(format, args)));
         }
 
         public new void Warn(string format, params object[] args)
         {
+            if (!AppSettings.Instance.Log.IsWarnEnabled) return;
             base.Warn(format, args);
             LoggrNetLogger.LogWarning(new LogMsg(string.Format(format, args)));
         }
 
         public void Troll(string format, params object[] args)
         {
+            if (!AppSettings.Instance.Log.IsTrollEnabled) return;
             LoggrNetLogger.LogTroll(new LogMsg(string.Format(format, args)));
         }
     }
--- WIO/WIO/Settings/LogSettings.cs
+++ WIO/WIO/Settings/LogSettings.cs
@@ -5,5 +5,12 @@
         public string LoggrApiKey { get; set; }
         public string LoggrLogKey { get; set; }
         public string LogglyInputKey { get; set; }
+
+        public bool IsDebugEnabled { get; set; }
+        public bool IsInfoEnabled { get; set; }
+        public bool IsWarnEnabled { get; set; }
+        public bool IsErrorEnabled { get; set; }
+        public bool IsFatalEnabled { get; set; }
+        public bool IsTrollEnabled { get; set; }
     }
 }
